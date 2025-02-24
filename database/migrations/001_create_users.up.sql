b16cb1ee4aa24e99835264a8e9680502cae6f3b1
RoCTH01 <cdrobby.chen@gmail.com>
2025-02-23 19:20:32 -0500

    uniq!!!!!!!

--- database/migrations/001_create_users.up.sql
+++ database/migrations/001_create_users.up.sql
@@ -2,7 +2,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE
     IF NOT EXISTS "User" (
-        "id" uuid NOT NULL DEFAULT uuid_generate_v4 (),
+        "id" uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
         "oauth_id" varchar(128) UNIQUE,
         "role" varchar(128) NOT NULL DEFAULT 'client', -- can be: 'client', 'artist', 'admin', 'super_admin'
         "username" varchar(128) NOT NULL,
